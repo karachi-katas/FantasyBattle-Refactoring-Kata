babbd003f5fb7b0d6ac939b29b08f8d0389df3c2
EmadBinAbid <emad.bin.abid@gmail.com>
2020-08-08 15:14:45 +0500

    Added calculateDamage test case.

--- src/test/java/codingdojo/PlayerTest.java
+++ src/test/java/codingdojo/PlayerTest.java
@@ -3,7 +3,9 @@ package codingdojo;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
@@ -23,13 +25,27 @@ public class PlayerTest {
     }
 
     // choose this one if you are not familiar with mocks
-    @Ignore("Test is not finished yet")
+    //    @Ignore("Test is not finished yet")
     @Test
     public void damageCalculations() {
-        Inventory inventory = new Inventory(null);
+        Item leftHand = new BasicItem("round shield", 0, (float)1.4);
+        Item rightHand = new BasicItem("excalibur", 20, (float)1.5);
+        Item head = new BasicItem("helmet", 0, (float)1.2);
+        Item feet = new BasicItem("boots", 0, (float)0.1);
+        Item chest = new BasicItem("steel plate", 0, (float)1.4);
+
+        Equipment equipment = new Equipment(leftHand, rightHand, head
+        , feet, chest);
+
+        Inventory inventory = new Inventory(equipment);
         Stats stats = new Stats(0);
-        SimpleEnemy target = new SimpleEnemy(null, null);
+
+        Armor armor = new SimpleArmor(5);
+        List<Buff> buffList = new ArrayList<Buff>();
+        buffList.add(new BasicBuff(1.0f, 1.0f));
+
+        SimpleEnemy target = new SimpleEnemy(armor, buffList);
         Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
+        assertEquals(102, damage.getAmount());
     }
 }
