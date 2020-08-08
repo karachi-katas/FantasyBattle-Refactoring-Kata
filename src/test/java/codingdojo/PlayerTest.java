c14ec3ffa01d7b7745e6ef5fc9e3015f997df7bf
MutibAkbar <mutibakbar2017@gmail.com>
2020-08-08 15:10:05 +0500

    Remove null pointer exception from test

--- src/test/java/codingdojo/PlayerTest.java
+++ src/test/java/codingdojo/PlayerTest.java
@@ -4,6 +4,8 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
@@ -28,11 +30,15 @@ public class PlayerTest {
     public void damageCalculations() {
         Item sword = new BasicItem("sword", 10, 2);
         Item helmet = new BasicItem("helmet", 10, 2);
+        Item chestplate = new BasicItem("chestplate", 10, 2);
         Item boots = new BasicItem("boots", 10, 2);
-        Equipment equipment = new Equipment(sword, sword, helmet, boots);
+        Equipment equipment = new Equipment(sword, sword, helmet, boots,chestplate);
         Inventory inventory = new Inventory(equipment);
         Stats stats = new Stats(0);
-        SimpleEnemy target = new SimpleEnemy(null, null);
+        Armor armor = new SimpleArmor(10);
+        Buff buff = new BasicBuff(10,10);
+        List<Buff> buffList = Collections.singletonList(buff);
+        SimpleEnemy target = new SimpleEnemy(armor, buffList);
         Damage damage = new Player(inventory, stats).calculateDamage(target);
         assertEquals(10, damage.getAmount());
     }
